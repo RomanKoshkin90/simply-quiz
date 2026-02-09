a414e7ef99a71d9f09b698ae89bf9800dc61e85e
ArtemBelykh <stell_artem@mail.ru>
2026-02-09 13:15:09 +0300

    feat: add yandex metriks

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -71,50 +71,7 @@ function App() {
       
       <Header onReset={handleReset} showReset={stage !== 'upload'} />
 
-      {/* Yandex.Metrika counter */}
-                {process.env.NODE_ENV === 'production' && (
-                    <>
-                        {/*<script src="//code.jivo.ru/widget/m8GsemgcCF" async></script>*/}
-                        <YandexHit />
-                        <Script
-                            id="yandex-metrika"
-                            type="text/javascript"
-                            strategy="afterInteractive"
-                        >
-                            {`
-                        (function(m,e,t,r,i,k,a){
-                            m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-                            m[i].l=1*new Date();
-                            for (var j = 0; j < document.scripts.length; j++) {
-                                if (document.scripts[j].src === r) { return; }
-                            }
-                            k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
-                        })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=104746311', 'ym');
-
-                        ym(104746311, 'init', {
-                            ssr:true,
-                            webvisor:true,
-                            clickmap:true,
-                            ecommerce:"dataLayer",
-                            referrer: document.referrer,
-                            url: location.href,
-                            accurateTrackBounce:true,
-                            trackLinks:true
-                        });
-`}
-                        </Script>
-                        <noscript>
-                            <div>
-                                <img
-                                    src="https://mc.yandex.ru/watch/104746311"
-                                    style={{ position: 'absolute', left: '-9999px' }}
-                                    alt=""
-                                />
-                            </div>
-                        </noscript>
-                        {/* /Yandex.Metrika counter */}
-                    </>
-                )}
+      <YandexHit />
       
       <main className="container mx-auto px-4 py-8">
         {/* Переключатель */}
